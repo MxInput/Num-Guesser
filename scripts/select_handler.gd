32fd57601e34a547e1b2f3ccffb7b85a345918c6
MxInput <mxinput@protonmail.com>
2026-09-12 18:41:12 -0400

    congratulates winning player

--- scripts/select_handler.gd
+++ scripts/select_handler.gd
@@ -23,6 +23,9 @@ const NUM_CHANCES := 6
 
 @export var num_gen : Node
 
+@export var congratualations_teller : RichTextLabel
+@export var lose_teller : RichTextLabel
+
 var all_correct := false
 
 enum NumStatus {
@@ -97,21 +100,39 @@ func change_appearance(statuses) -> void:
 		count += 1
 	
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed and !game_over():
+	if event is InputEventKey and event.pressed:
 		var keycode = DisplayServer.keyboard_get_keycode_from_physical(event.physical_keycode);
 		
-		if (keycode == Key.KEY_ENTER):
-			if (all_filled()):
-				chances_left -= 1
-				chances_text.text = "Chances Left: " + str(chances_left) + "/" + str(NUM_CHANCES)
-				
-				var generated_numbers = num_gen.numbers
-				
-				var statuses = compare_numbers(generated_numbers)
-				change_appearance(statuses)
-				
-				if (game_over()):
-					if (!all_correct):
+		if (keycode == Key.KEY_R):
+			get_tree().reload_current_scene()
+			
+		if !game_over():	
+			if (keycode == Key.KEY_ENTER):
+				if (all_filled()):
+					chances_left -= 1
+					chances_text.text = "Chances Left: " + str(chances_left) + "/" + str(NUM_CHANCES)
+					
+					var generated_numbers = num_gen.numbers
+					
+					var statuses = compare_numbers(generated_numbers)
+					change_appearance(statuses)
+					
+					if (game_over()):
+						if (!all_correct):
+							lose_teller.visible = true
+							
+							var new_container = num_containers.duplicate()
+							num_containers.get_parent().add_child(new_container)
+							new_container.position.y += new_container.get_child(0).size.y
+						
+							num_gen.num_containers = new_container
+							num_gen.default_containers()
+							num_containers = new_container
+						
+							fill_in_correct(generated_numbers)
+						else:
+							congratualations_teller.visible = true
+					else:
 						var new_container = num_containers.duplicate()
 						num_containers.get_parent().add_child(new_container)
 						new_container.position.y += new_container.get_child(0).size.y
@@ -119,26 +140,45 @@ func _input(event: InputEvent) -> void:
 						num_gen.num_containers = new_container
 						num_gen.default_containers()
 						num_containers = new_container
-					
-						fill_in_correct(generated_numbers)
-				else:
-					var new_container = num_containers.duplicate()
-					num_containers.get_parent().add_child(new_container)
-					new_container.position.y += new_container.get_child(0).size.y
+						
+					current_container_index = 0
+			else:
+				var typed_event = event as InputEventKey;
+				var representation : String = PackedByteArray([typed_event.unicode]).get_string_from_utf8();
 				
-					num_gen.num_containers = new_container
-					num_gen.default_containers()
-					num_containers = new_container
+				if (representation.is_valid_int()):
+					var current_container = num_containers.get_children()[current_container_index]
+					current_container.get_child(0).text = representation
 					
-				current_container_index = 0
-		else:
-			var typed_event = event as InputEventKey;
-			var representation : String = PackedByteArray([typed_event.unicode]).get_string_from_utf8();
-			
-			if (representation.is_valid_int()):
+					current_container.color = default_color
+					
+					current_container_index += 1
+					
+					if (current_container_index >= NUM_OF_CONTAINERS):
+						current_container_index = 0
+						
+					var new_container = num_containers.get_children()[current_container_index]
+					new_container.color = selected_color
+				
+		if (event.is_action("left") and not game_over()):
+			if (delay.is_stopped()):
+				delay.start()
+				
 				var current_container = num_containers.get_children()[current_container_index]
-				current_container.get_child(0).text = representation
+				current_container.color = default_color
 				
+				current_container_index -= 1
+				
+				if (current_container_index < 0):
+					current_container_index = NUM_OF_CONTAINERS - 1
+					
+				var new_container = num_containers.get_children()[current_container_index]
+				new_container.color = selected_color
+		if (event.is_action("right") and not game_over()):
+			if (delay.is_stopped()):
+				delay.start()
+				
+				var current_container = num_containers.get_children()[current_container_index]
 				current_container.color = default_color
 				
 				current_container_index += 1
@@ -148,33 +188,4 @@ func _input(event: InputEvent) -> void:
 					
 				var new_container = num_containers.get_children()[current_container_index]
 				new_container.color = selected_color
-			
-	if (event.is_action("left") and not game_over()):
-		if (delay.is_stopped()):
-			delay.start()
-			
-			var current_container = num_containers.get_children()[current_container_index]
-			current_container.color = default_color
-			
-			current_container_index -= 1
-			
-			if (current_container_index < 0):
-				current_container_index = NUM_OF_CONTAINERS - 1
-				
-			var new_container = num_containers.get_children()[current_container_index]
-			new_container.color = selected_color
-	if (event.is_action("right") and not game_over()):
-		if (delay.is_stopped()):
-			delay.start()
-			
-			var current_container = num_containers.get_children()[current_container_index]
-			current_container.color = default_color
-			
-			current_container_index += 1
-			
-			if (current_container_index >= NUM_OF_CONTAINERS):
-				current_container_index = 0
-				
-			var new_container = num_containers.get_children()[current_container_index]
-			new_container.color = selected_color
 		
