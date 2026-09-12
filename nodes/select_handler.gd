f0088599738f1dc266a692b12834e0b1d29b8205
MxInput <mxinput@protonmail.com>
2026-09-12 13:58:26 -0400

    user can type numbers

--- nodes/select_handler.gd
+++ nodes/select_handler.gd
@@ -5,14 +5,47 @@ extends Node
 var current_container_index := 0
 const NUM_OF_CONTAINERS := 4
 
+@export var default_color : Color;
+@export var selected_color : Color;
+
+@export var delay : Timer
+
+
 func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		var typed_event = event as InputEventKey;
+		var representation : String = PackedByteArray([typed_event.unicode]).get_string_from_utf8();
+		
+		if (representation.is_valid_int()):
+			var current_container = num_containers.get_children()[current_container_index]
+			current_container.get_child(0).text = representation
+			
 	if (event.is_action("left")):
-		current_container_index -= 1
-		
-		if (current_container_index < 0):
-			current_container_index = NUM_OF_CONTAINERS - 1
+		if (delay.is_stopped()):
+			delay.start()
+			
+			var current_container = num_containers.get_children()[current_container_index]
+			current_container.color = default_color
+			
+			current_container_index -= 1
+			
+			if (current_container_index < 0):
+				current_container_index = NUM_OF_CONTAINERS - 1
+				
+			var new_container = num_containers.get_children()[current_container_index]
+			new_container.color = selected_color
 	if (event.is_action("right")):
-		current_container_index += 1
+		if (delay.is_stopped()):
+			delay.start()
+			
+			var current_container = num_containers.get_children()[current_container_index]
+			current_container.color = default_color
+			
+			current_container_index += 1
+			
+			if (current_container_index >= NUM_OF_CONTAINERS):
+				current_container_index = 0
+				
+			var new_container = num_containers.get_children()[current_container_index]
+			new_container.color = selected_color
 		
-		if (current_container_index >= NUM_OF_CONTAINERS):
-			current_container_index = 0
--- scripts/number_generator.gd
+++ scripts/number_generator.gd
@@ -3,7 +3,26 @@ extends Node
 var numbers := []
 const AMOUNT_TO_GEN := 4
 
+@export var default_color : Color;
+@export var selected_color : Color;
+
+@export var num_containers : Control
+
+func default_containers() -> void:
+	var first = true
+	
+	for container in num_containers.get_children():
+		if (first):
+			first = false
+			container.color = selected_color
+		else:
+			container.color = default_color
+			
+		container.get_child(0).text = ""
+	
 func generate_new_numbers() -> void:
+	default_containers()
+	
 	numbers.clear()
 	
 	for current_index in AMOUNT_TO_GEN:
